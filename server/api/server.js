039a67dbad059fcfb831ff1c717029e592b2b120
MuneebAmdani <arhammuneeb239@gmail.com>
2025-08-09 18:41:49 +0500

    testing

--- server/api/server.js
+++ server/api/server.js
@@ -1,36 +1,10 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db.js');
 const serverless = require('serverless-http');
 
-const authRoutes = require('./routes/authRoutes.js');
-const userRoutes = require('./routes/userRoutes.js');
-const beneficiaryRoutes = require('./routes/beneficiaryRoutes.js');
-const statsRoutes = require('./routes/statsRoutes.js');
-
-dotenv.config();
-
 const app = express();
-
-app.use(cors({
-  origin: ['https://beneficiary-management-system-ten.vercel.app'],
-  credentials: true,
-}));
-
-app.use(express.json());
-
-connectDB();
-
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/beneficiaries', beneficiaryRoutes);
-app.use('/api/stats', statsRoutes);
 
 app.get('/api/health', (req, res) => {
   res.json({ success: true, message: 'Server running' });
 });
-
-// no app.listen() here for serverless
 
 module.exports = serverless(app);
