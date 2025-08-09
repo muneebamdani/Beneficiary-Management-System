191570a797d5643c2264aee0196478f22f50e1f2
MuneebAmdani <arhammuneeb239@gmail.com>
2025-08-09 18:54:51 +0500

    now

--- server/api/server.js
+++ server/api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db.js');
+const connectDB = require('../config/db.js');
 const serverless = require('serverless-http');
 
-const authRoutes = require('./routes/authRoutes.js');
-const userRoutes = require('./routes/userRoutes.js');
-const beneficiaryRoutes = require('./routes/beneficiaryRoutes.js');
-const statsRoutes = require('./routes/statsRoutes.js');
+const authRoutes = require('../routes/authRoutes.js');
+const userRoutes = require('../routes/userRoutes.js');
+const beneficiaryRoutes = require('../routes/beneficiaryRoutes.js');
+const statsRoutes = require('../routes/statsRoutes.js');
 
 dotenv.config();
 
