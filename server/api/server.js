419662d05f4174d7db28bc22fe90d355fde4cd59
MuneebAmdani <arhammuneeb239@gmail.com>
2025-08-10 19:44:32 +0500

    port

--- server/api/server.js
+++ server/api/server.js
@@ -35,6 +35,8 @@ const startServer = async () => {
     });
 
     const PORT = process.env.PORT || 5000;
+    console.log('PORT from env:', process.env.PORT);
+
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
